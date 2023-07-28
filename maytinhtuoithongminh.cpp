d33384800359c19a9b00dfbe388f3ef0c9f55b93
VuGiaThinh <vuthinhqaz123@gmail.com>
2023-07-28 13:22:27 +0700

    init project

---
+++ maytinhtuoithongminh.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Nhap so tuoi cua ban: ";
+    cin>>age;
+    cout<<"Tuoi cua ban la: "<<age;
+    
+    return 0;
+}
